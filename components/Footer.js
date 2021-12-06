f28d41761365752f65bc54ec42745fa3a5773083
mamascor <75904844+mamascor@users.noreply.github.com>
2021-12-06 17:56:50 -0600

    Add files via upload

--- components/Footer.js
+++ components/Footer.js
@@ -21,7 +21,7 @@ function Footer() {
 
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">HOST</h5>
-        <p>Papa React</p>
+        <p>Marco Website</p>
         <p>Presents</p>
         <p>Zero to Full Stack Hero</p>
         <p>Hundreds of Students</p>
